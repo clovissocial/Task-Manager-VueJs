fd45e6b87cf3f145322a86ad40384c49bb3b2ccc
clovissocial <sociale965@gmail.com>
2021-06-18 16:48:48 +0100

    first commit

---
+++ src/components/Task.vue
@@ -0,0 +1,44 @@
+<template>    
+    <div @click="$emit('toggle-reminder', task.id)" 
+    :class="[task.reminder ? 'reminder' : '', 'task']">
+        <h3>{{ task.text }}
+            <i @dblclick="$emit('delete-task', task.id)" class="fas fa-times"></i>
+        </h3>
+        <p>{{ task.day }}</p>
+    </div> 
+</template>
+
+<script>
+
+export default {
+    name: 'Task',
+    props: {
+        task: Object,
+    }, 
+}
+</script>
+
+<style scoped>
+
+    .task {
+        background: #f4f4f4;
+        margin: 5px;
+        padding: 10px;
+        cursor: pointer;
+    }
+
+    .task.reminder {
+        border-left: 5px solid green;
+    }
+
+    .task h3 {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+
+    .fas {
+        color: red;
+    }
+
+</style>
